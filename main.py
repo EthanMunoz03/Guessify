15915de1c9c1368e082315fce35ac1fc55608f98
Ethan Munoz <notrealethan@gmail.com>
2024-07-19 21:58:02 +0000

    added style to templates

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', subtitle='Guess the song name!', text='Click on the play button to replay')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
